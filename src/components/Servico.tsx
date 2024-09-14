6196434f135d3331e49fbe3fa4bf550e47a7f4d1
AntonioGuiDev <antoniooguii@gmail.com>
2024-09-14 12:37:32 -0300

    props serviços

--- src/components/Servico.tsx
+++ src/components/Servico.tsx
@@ -3,25 +3,25 @@ import desbloqueio from "./img/servico2.jpg"
 import bateria from "./img/servico3.jpg"
 
 
-function Servicos(){
+function Servicos(props:ServicosProps){
     return(
         <>
         
         <h1>SERVIÇOS</h1>
         <div>
-            <h2>CONSERTO DE TELAS</h2>
-            <p>Realizamos conserto de telas em nossas lojas, a partir de R$80,00</p>
+            <h2>{props.nome1}</h2>
+            <p>{props.descricao1}</p>
             <img src={tela} alt="Conserto de tela" />
 
         </div>
         <div>
-            <h2>DESBLOQUEIO DE CELULAR</h2>
-            <p>Realizamos o desbloqueio de celulares, apenas com a nota fiscal do aparelho, a partir de R$100,00</p>
+            <h2>{props.nome2}</h2>
+            <p>{props.descricao2}</p>
             <img src={desbloqueio} alt="Desbloqueio de celular" />
         </div>
         <div>
-            <h2>REPARO DE BARERIA</h2>
-            <p>Realizamos a troca de baterias com problemas de duração, a partir de R$150,00</p>
+            <h2>{props.nome3}</h2>
+            <p>{props.descricao3}</p>
             <img src={bateria} alt="Conserto de bateria" />
         </div>
         
--- src/types.ts
+++ src/types.ts
@@ -21,3 +21,22 @@ export type Aparelho = {
   imagem: string;
 
 }
+
+export type Servico = {
+  nome: string;
+  descricao: string;
+  preco: number;
+  imagem: string;
+}
+
+export type ServicoProps = {
+  nome1: "CONSERTO DE TELAS"
+  descricao1:"Realizamos conserto de telas em nossas lojas, a partir de R$80,00"
+
+  nome2: "DESBLOQUEIO DE CELULAR"
+  descricao2:"Realizamos o desbloqueio de celulares, apenas com a nota fiscal do aparelho, a partir de R$100,00"
+
+  nome3: "REPARO DE BARERIA"
+  descricao3:"Realizamos a troca de baterias com problemas de duração, a partir de R$150,00"
+}
+
